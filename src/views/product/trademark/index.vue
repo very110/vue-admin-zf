d482ed35ac313cd63c4b965c4b13509637b81e45
very110 <1833147337@qq.com>
2023-08-02 21:40:39 +0800

    8.2

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,8 +1,13 @@
 <template>
     <div >
-        <TablePlus :data="tableData" :config="tableConfig"  ref="tabplus">
-        </TablePlus>
-
+       <el-card>
+           <el-button type="primary">添加品牌</el-button>
+           <TablePlus :data="tableData" :config="tableConfig" :paginationConfig="paginationConfig"
+                      border style="margin:20px 0"
+                      :row-style="{'height':'100px'}"
+           >
+           </TablePlus>
+       </el-card>
     </div>
 </template>
 
@@ -22,54 +27,60 @@ const tableConfig: TableConfig<TableData> = reactive({
         {
             types: "txt",
             prop: "address",
-            label: '地址',
+            label: '序号',
+            width:100
         } ,
         {
             types: "txt",
             prop: "date",
-            label: '日期',
+            label: '品牌名称',
         },
         {
-            types: "callback",
+            types: "txt",
             prop: "name",
-            label: '名字',
-            width:100,
-            callback:(row)=>{
-                return row.name
-            }
+            label: '品牌LoGo',
+        },
+        {
+            types: "txt",
+            prop: "name",
+            label: '品牌操作',
         }
     ]
 })
-
+const paginationConfig=reactive({
+    total:200,
+    events:{
+        'prev-click':()=>{
+            console.log('prev-click')
+        }
+    }
+})
 const tableData:TableData[] = [
     {
         date: '2016-05-03',
         name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles',
+        address: 's',
     },
     {
         date: '2016-05-02',
         name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles',
+        address: 's',
     },
     {
         date: '2016-05-04',
         name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles',
+        address: 's',
     },
     {
         date: '2016-05-01',
         name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles',
+        address: 's',
     }
 ]
 
-const tabplus=ref();
 
 </script>
 
 
 <style scoped lang="scss">
-
-
 </style>
